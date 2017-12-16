3f48dd9970592d35101128c31e7b5fa54d55d952
sebiba <sebiba@gmail.com>
2017-12-16 15:00:10 +0100

    Add files via upload
    work on it

---
+++ mvc/launch/blackjackMVC.java
@@ -0,0 +1,49 @@
+package test;
+/**
+ * @author <a href="mailto:gery.casiez@lifl.fr">Gery Casiez</a>
+ * modified by vvds
+ */
+
+import controller.GameController;
+import model.Game;
+import view.blackjackVue;
+import view.blackjackVueConsole;
+//import view.blackjackVueGUI;
+
+public class blackjackMVC {
+	public blackjackMVC() {
+		//Création du modèle
+//Player model1 = new Player();
+		Game model = new Game();
+		//TODO
+
+		//Création des contrôleurs : Un pour chaque vue
+		//Chaque contrôleur doit avoir une référence vers le modèle pour pouvoir le commander
+//GameController ctrlGUI = new GameController(model);
+		GameController ctrlConsole = new GameController(model);
+		//TODO
+		
+		//Création des vues.
+		//Chaque vue doit connaître son contrôleur et avoir une référence vers le modèle pour pouvoir l'observer
+//blackjackVue gui = new blackjackVueGUI(model, ctrlGUI, 200, 200);
+		blackjackVue console = new blackjackVueConsole(model, ctrlConsole);
+		//TODO
+		
+		//On donne la référence à la vue pour chaque contrôleur
+//ctrlGUI.addView(gui);
+		ctrlConsole.addView(console);
+		//TODO
+		
+		
+	}
+	
+	public static void main(String args[]) {
+		//Schedule a job for the event-dispatching thread:
+		//creating and showing this application's GUI.
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				new blackjackMVC();
+			}
+		});
+	}
+}
