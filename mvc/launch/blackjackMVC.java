1b2e38a5cc5c3478c4416f9ec480adccc5f6803b
sebiba <sebiba@gmail.com>
2017-12-20 18:06:26 +0100

    Add files via upload
    mvc is working

--- mvc/launch/blackjackMVC.java
+++ mvc/launch/blackjackMVC.java
@@ -1,44 +1,33 @@
-package test;
+package main;
+
+import java.awt.EventQueue;
 
 import controller.GameController;
 import model.Game;
-import view.blackjackVue;
-import view.blackjackVueConsole;
-import view.blackjackVueGUI;
+import view.BlackjackVue;
+import view.BlackjackVueConsole;
 
 public class blackjackMVC {
+	BlackjackVueConsole view;
 	public blackjackMVC() {
-		//Création du modèle
 		Game model = new Game();
-		//TODO
-
-		//Création des contrôleurs : Un pour chaque vue
-		//Chaque contrôleur doit avoir une référence vers le modèle pour pouvoir le commander
-		GameController ctrlGUI = new GameController(model);
-		GameController ctrlConsole = new GameController(model);
-		//TODO
 		
-		//Création des vues.
-		//Chaque vue doit connaître son contrôleur et avoir une référence vers le modèle pour pouvoir l'observer
-		blackjackVue gui = new blackjackVueGUI(model, ctrlGUI, 200, 200);
-		blackjackVue console = new blackjackVueConsole(model, ctrlConsole);
-		//TODO
+		GameController controlCli = new GameController(model);
 		
-		//On donne la référence à la vue pour chaque contrôleur
-		ctrlGUI.addView(gui);
-		ctrlConsole.addView(console);
-		//TODO
-		
-		
+		BlackjackVue cli = new BlackjackVueConsole(model, controlCli);
+		controlCli.addView(cli);
+		cli.menu();
 	}
 	
 	public static void main(String args[]) {
 		//Schedule a job for the event-dispatching thread:
 		//creating and showing this application's GUI.
-		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+
+		new blackjackMVC();
+		/*EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				new blackjackMVC();
 			}
-		});
+		});*/
 	}
 }
