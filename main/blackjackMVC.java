f2f2be4960457231242b91eed5e9ea2bfa5e4113
HumbertMeyers <HumbertMeyers@users.noreply.github.com>
2017-12-21 22:14:22 +0100

    update blackjackMVC.java
    add Javadoc

--- main/blackjackMVC.java
+++ main/blackjackMVC.java
@@ -9,6 +9,14 @@ import view.BlackjackVueConsole;
 import view.BlackjackVueGui;
 
 public class blackjackMVC {
+	/**
+	 * constructeur initialisant 
+	 * un model
+	 * deux controlleurs pour chaque vues
+	 * une vue console prenant en parametre le model et un des controlleurs
+	 * une vue graphique prenant en parametre le model et un des controlleurs
+	 * lie les deux controlleurs a leur vue respective
+	 */
 	public blackjackMVC() {
 		Game model = new Game();
 		
@@ -21,9 +29,14 @@ public class blackjackMVC {
 		controlCli.addView(cli);
 		controlGui.addView(gui);
 		
+		gui.menu();
 		cli.menu();
 	}
 	
+	/**
+	 * fonction principale du jeux
+	 * @param args arguments donnés au programme
+	 */
 	public static void main(String args[]) {
 		//Schedule a job for the event-dispatching thread:
 		//creating and showing this application's GUI.
